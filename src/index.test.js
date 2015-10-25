87538e59c716a256c3d852c7375463114cb945d6
Vasa <vasanth.krishnamoorthy@citrix.com>
2015-10-25 14:00:33 -0700

    Added meaningful tests

--- src/index.test.js
+++ src/index.test.js
@@ -2,7 +2,26 @@ var expect = require('chai').expect;
 var starWars = require('./index');
 
 describe('starwars-names', function () {
-  it('should work', function () {
-    expect(true).to.be.true;
+  describe('all', function () {
+    it('should be an array of strings', function () {
+      expect(starWars.all).to.satisfy(isArrayOfStrings);
+
+      function isArrayOfStrings(array) {
+        return array.every(function (item) {
+          return typeof item === 'string';
+        });
+      }
+    });
+
+    it('should contain "Vasa"', function() {
+      expect(starWars.all).to.include('Vasa');
+    })
   });
+
+  describe('random', function() {
+    it('should return a random item from the starWars.all', function() {
+      var randomCharacter = starWars.random();
+      expect(starWars.all).to.include(randomCharacter);
+    })
+  })
 });
